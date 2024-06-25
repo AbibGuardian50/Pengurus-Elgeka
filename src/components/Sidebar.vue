744b6738ef30da168228b4f8eead158f20ee7aaf
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-06-25 14:44:27 +0700

    ✨ feat: Responsive & Navbar
    Adding responsive to sidebar and dataumumpasien

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,7 +1,17 @@
 <script>
 import VueCookies from 'vue-cookies';
 export default {
+    data() {
+        return {
+            open: false,
+            dimmer: true,
+            right: false
+        };
+    },
     methods: {
+        toggle() {
+            this.open = !this.open;
+        },
         logout() {
             // Hapus token dari localStorage atau dari state aplikasi
             // VueCookies.remove('tokenlogin'); // Contoh jika token disimpan di localStorage atau cookies
@@ -19,17 +29,25 @@ export default {
 </script>
 
 <template>
-    <div class="flex">
-        <div class="bg-teal flex flex-col items-center pt-8 w-[310px] h-full">
-            <div>
-                <div class="flex justify-center items-center">
-                    <img class="mb-8 rounded-md" src="../assets/logoElgekaPutih1.png" alt="">
+    <!-- > 768 px screen and laptop screen -->
+    <div class="flex max-[520px]:max-w-[230px] max-[520px]:min-w-[25%] max-md:hidden">
+        <div
+            class="bg-teal flex flex-col items-center pt-8 md:w-full max-[520px]:min-w-[70px] min-w-[70px] max-[1800px]:h-full h-screen">
+            <div class="flex flex-col items-center">
+                <img class="mb-8 rounded-md flex items-center justify-center max-md:w-[60px] max-md:h-[60px]"
+                    src="../assets/logoElgekaPutih1.png" alt="">
+                <svg xmlns="http://www.w3.org/2000/svg" id="menu-button" @click="toggleHamburgerMenu"
+                    class="h-6 w-6 cursor-pointer min-[965px]:hidden block border border-white w-[40px] rounded-md h-[40px]"
+                    fill="none" viewBox="0 0 24 24" stroke="#FFFFFF">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+                <div class="flex flex-col items-start ml-4 mr-32 max-[420px]:mr-4 max-[420px]:text-[14px] max-[420px]:mt-4">
+                    <p class="text-left font-poppins font-semibold  text-white mr-12 hidden min-[965px]:block"
+                        id="menu">General</p>
                 </div>
-                <div class="flex flex-col items-start mr-32">
-                    <p class="text-left font-gotham font-semibold text-white mr-12">General</p>
-                </div>
-                <div>
-                    <div class="flex flex-col gap-2 pt-4">
+
+                <div class="hidden min-[965px]:block" id="menu-sidebar">
+                    <div class="flex flex-col gap-2 pt-4 max-[420px]:text-[12px]">
                         <ul class="flex flex-col gap-2">
                             <li>
                                 <details class="group">
@@ -164,12 +182,195 @@ export default {
 
             <div class="flex justify-start mt-auto max-[1800px]:pt-12 items-start pb-8">
                 <router-link to="/">
-                    <div class="flex justify-start gap-2">
+                    <div @click="logout" class="flex justify-start gap-2">
                         <img src="../assets/logout-logo.png" alt="Logo Logout">
-                        <p @click="logout" class="text-white font-bold text-base">Keluar</p>
+                        <p class="text-white font-inter font-bold text-base max-[965px]:hidden">Keluar</p>
                     </div>
                 </router-link>
             </div>
         </div>
+    </div>
+
+    <!-- 768 px screen < and mobile -->
+    <div class="flex md:hidden">
+        <!-- Sidebar -->
+        <div class="flex top-0 h-screen z-20 bg-teal" :class="[right ? 'right-0 flex-row' : 'left-0 flex-row-reverse']">
+            <!--Drawer -->
+            <span class="block transform origin-center font-bold flex flex-col items-center w-[60px]">
+                <img class="mb-8 rounded-md flex items-center justify-center max-md:w-[50px] max-md:h-[50px]"
+                    src="../assets/logoElgekaPutih1.png" alt="">
+                <svg xmlns="http://www.w3.org/2000/svg" id="menu-button" @click.prevent="toggle()"
+                    class="h-6 w-6 cursor-pointer min-[965px]:hidden block border border-white w-[35px] rounded-md h-[35px]"
+                    fill="none" viewBox="0 0 24 24" stroke="#FFFFFF">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
+                </svg>
+
+                <div class="flex justify-start mt-auto max-[1800px]:pt-12 items-start pb-8">
+                    <router-link to="/">
+                        <div @click="logout" class="flex justify-start gap-2">
+                            <img src="../assets/logout-logo.png" alt="Logo Logout">
+                            <p class="text-white font-inter font-bold text-base max-[965px]:hidden">Keluar</p>
+                        </div>
+                    </router-link>
+                </div>
+            </span>
+            <!-- Sidebar Content -->
+            <div ref="content" class="transition-all duration-700 overflow-hidden" :class="[open ? 'max-w-lg' : 'max-w-0']">
+                <div class="w-48 text-center font-bold text-xl">
+                    <div class="">
+                        <div
+                            class="bg-teal flex flex-col items-center md:w-full max-[520px]:min-w-[70px] min-w-[70px] max-[1800px]:h-full h-screen">
+                            <div class="flex flex-col items-center">
+                                <div
+                                    class="flex flex-col items-start ml-4 max-[420px]:mr-4 max-[420px]:text-[14px] max-[420px]:mt-4">
+                                    <p class="text-center font-poppins font-semibold  text-white block" id="menu">
+                                        General</p>
+                                </div>
+
+                                <div class="block" id="menu-sidebar">
+                                    <div class="flex flex-col gap-2 pt-4 max-[420px]:text-[12px] max-md:text-[14px]">
+                                        <ul class="flex flex-col gap-2">
+                                            <li>
+                                                <details class="group">
+                                                    <summary
+                                                        class="flex gap-2 items-center justify-between font-medium marker:content-none hover:text-midnightshadow py-2  hover:cursor-pointer ">
+                                                        <span
+                                                            class="flex gap-2 hover:text-midnightshadow hover:bg-white focus:text-midnightshadow">
+                                                            <span
+                                                                class="w-full hover:text-midnightshadow focus:text-midnightshadow focus:bg-[#EEEEED] px-4 focus:text-black  focus:font-bold font-bold text-white font-inter">
+                                                                Data Umum
+                                                            </span>
+                                                        </span>
+                                                        <svg width="25" height="24" viewBox="0 0 25 24" fill="none"
+                                                            xmlns="http://www.w3.org/2000/svg">
+                                                            <path
+                                                                d="M18.75 10L13.2366 15.2929C12.8298 15.6834 12.1702 15.6834 11.7634 15.2929L6.25 10"
+                                                                stroke="white" stroke-width="2" stroke-linecap="round" />
+                                                        </svg>
+
+                                                    </summary>
+                                                    <article class="px-4 pb-4 bg-whisperingsilver rounded-lg">
+                                                        <ul class="flex flex-col gap-4 pl-2 mt-4 pt-2">
+                                                            <li
+                                                                class="flex gap-2 font-bold font-inter text-[16px] leading-5 text-midnightshadow">
+                                                                <a href="/dataumumpasien">
+                                                                    Data Pasien
+                                                                </a>
+                                                            </li>
+                                                            <li
+                                                                class="flex gap-2 font-bold font-inter text-[16px] leading-5 text-midnightshadow">
+                                                                <a href="/dataumumdokter">
+                                                                    Data Dokter
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </article>
+                                                </details>
+                                            </li>
+                                        </ul>
+
+                                        <ul class="flex flex-col gap-2">
+                                            <li>
+                                                <details class="group">
+                                                    <summary
+                                                        class="flex gap-2 items-center justify-between font-medium marker:content-none hover:text-midnightshadow py-2  hover:cursor-pointer ">
+                                                        <span
+                                                            class="flex gap-2 hover:text-midnightshadow hover:bg-white focus:text-midnightshadow">
+                                                            <span
+                                                                class="w-full hover:text-midnightshadow focus:text-midnightshadow focus:bg-[#EEEEED] px-4 focus:text-black  focus:font-bold font-bold text-white font-inter">
+                                                                Data Hasil Lab
+                                                            </span>
+                                                        </span>
+                                                        <svg width="25" height="24" viewBox="0 0 25 24" fill="none"
+                                                            xmlns="http://www.w3.org/2000/svg">
+                                                            <path
+                                                                d="M18.75 10L13.2366 15.2929C12.8298 15.6834 12.1702 15.6834 11.7634 15.2929L6.25 10"
+                                                                stroke="white" stroke-width="2" stroke-linecap="round" />
+                                                        </svg>
+
+                                                    </summary>
+                                                    <article class="px-4 pb-4 bg-whisperingsilver rounded-lg">
+                                                        <ul class="flex flex-col gap-4 pl-2 mt-4 pt-2">
+                                                            <li
+                                                                class="flex gap-2 font-bold font-inter text-[16px] leading-5 text-midnightshadow">
+                                                                <a href="/HasilLabBCRABL">
+                                                                    BCR-ABL
+                                                                </a>
+                                                            </li>
+                                                            <li
+                                                                class="flex gap-2 font-bold font-inter text-[16px] leading-5 text-midnightshadow">
+                                                                <a href="/HasilLabLeukocytes">
+                                                                    Leukosit
+                                                                </a>
+                                                            </li>
+                                                            <li
+                                                                class="flex gap-2 font-bold font-inter text-[16px] leading-5 text-midnightshadow">
+                                                                <a href="/HasilLabPotentialHydrogen">
+                                                                    Potential Hydrogen
+                                                                </a>
+                                                            </li>
+                                                            <li
+                                                                class="flex gap-2 font-bold font-inter text-[16px] leading-5 text-midnightshadow">
+                                                                <a href="/HasilLabHemoglobin">
+                                                                    Hemoglobin
+                                                                </a>
+                                                            </li>
+                                                            <li
+                                                                class="flex gap-2 font-bold font-inter text-[16px] leading-5 text-midnightshadow">
+                                                                <a href="/HasilLabHeartRate">
+                                                                    Heart Rate
+                                                                </a>
+                                                            </li>
+                                                            <li
+                                                                class="flex gap-2 font-bold font-inter text-[16px] leading-5 text-midnightshadow">
+                                                                <a href="/HasilLabBloodPressure">
+                                                                    Blood Pressure
+                                                                </a>
+                                                            </li>
+                                                        </ul>
+                                                    </article>
+                                                </details>
+                                            </li>
+                                        </ul>
+
+                                        <router-link to="/DataKepemilikanObat">
+                                            <button
+                                                class="focus:bg-[#EEEEED] px-4 focus:text-black  focus:font-bold font-bold text-white font-inter ">Kepemilikan
+                                                Obat</button>
+                                        </router-link>
+
+                                        <router-link to="/datarumahsakit">
+                                            <button
+                                                class="focus:bg-[#EEEEED] px-4 focus:text-black  focus:font-bold font-bold text-white font-inter ">Data
+                                                Rumah Sakit</button>
+                                        </router-link>
+
+                                        <router-link to="/kelolaakun">
+                                            <button
+                                                class="focus:bg-[#EEEEED] px-4 focus:text-black  focus:font-bold font-bold text-white font-inter ">Kelola
+                                                Akun</button>
+                                        </router-link>
+
+                                        <router-link to="/verifikasidokter">
+                                            <button
+                                                class="focus:bg-[#EEEEED] px-4 focus:text-black  focus:font-bold font-bold text-white font-inter ">Verifikasi
+                                                Dokter</button>
+                                        </router-link>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <slot></slot>
+            </div>
+        </div>
+
+        <transition name="fade">
+            <!-- Dimmer -->
+            <div v-if="dimmer && open" @click="toggle()"
+                class="flex-1 bg-gray-400 bg-opacity-75 active:outline-none z-10" />
+        </transition>
     </div>
 </template>
--- src/views/DataUmumPasien.vue
+++ src/views/DataUmumPasien.vue
@@ -330,78 +330,70 @@ export default {
     <div class="flex bg-offwhite">
         <Sidebar />
 
-        <div class="flex flex-col gap-4 pt-4 pl-4">
-            <div class="flex gap-4 items-center">
-                <div class="flex flex-col items-center justify-center gap-4 bg-white rounded-lg pl-4 py-4 pr-8">
-                    <p class="font-assistant text-[18px] font-bold leading-5 text-midnightblue w-full pt-4 pl-8">Grafik
-                        Pasien
-                        berdasarkan Kabupaten</p>
+        <div class="flex flex-col gap-4 pt-4 pl-4 w-full lg:w-auto max-md:w-[95%]">
+            <div class="flex flex-col gap-4 min-[1400px]:flex-row">
+                <div
+                    class="flex flex-col items-center justify-center gap-4 bg-white rounded-lg p-4 w-full max-[1400px]:w-[800px] max-[1000px]:max-w-[600px] max-md:w-[95%] lg:pl-8 lg:py-4 lg:pr-8">
+                    <p class="font-assistant text-[18px] font-bold leading-5 text-midnightblue w-full pt-4 lg:pl-8">Grafik
+                        Pasien berdasarkan Kabupaten</p>
                     <Bar v-if="loaded" :data="DistrictData" :options="DistrictOptions"
-                        class="border border-lightsilver rounded-md min-w-[800px] max-w-[900px] min-h-[400px] max-h-[450px] text-white ml-8 p-4" />
+                        class="border border-lightsilver rounded-md w-full lg:max-w-[900px] lg:min-h-[400px] lg:max-h-[450px] text-white p-4" />
                 </div>
 
                 <div
-                    class="flex flex-col justify-between pl-4 bg-work bg-no-repeat bg-center bg-cover h-full rounded-md max-h-[900px] min-w-[509px] max-w-[700px]">
+                    class="flex flex-col justify-between p-4 bg-work bg-no-repeat bg-center bg-cover rounded-md h-full max-h-[900px] w-full max-md:w-[95%] lg:w-auto lg:max-w-[700px]">
                     <div class="flex flex-col gap-4">
                         <p class="pt-8 font-opensans text-white font-bold text-[16px] leading-4">DATA UMUM PASIEN</p>
                         <p class="font-opensans text-white font-normal text-[16px] leading-4">Baca lebih lanjut tentang data
                             umum pasien</p>
                     </div>
-                    <div class="">
-                        <a href="/detaildataumumpasien"><button
-                                class="font-opensans text-white flex items-center gap-2 pb-4">Read
-                                more <svg width="12" height="11" viewBox="0 0 12 11" fill="none"
+                    <div class="pt-4">
+                        <a href="/detaildataumumpasien">
+                            <button class="font-opensans text-white flex items-center gap-2 pb-4">
+                                Read more
+                                <svg width="12" height="11" viewBox="0 0 12 11" fill="none"
                                     xmlns="http://www.w3.org/2000/svg">
                                     <path
                                         d="M1 6.00156H8.586L6.293 8.29456C6.03304 8.54563 5.92879 8.91743 6.0203 9.26706C6.11182 9.61669 6.38486 9.88974 6.73449 9.98125C7.08412 10.0728 7.45593 9.96851 7.707 9.70856L11.707 5.70856C11.8951 5.52095 12.0008 5.26621 12.0008 5.00056C12.0008 4.7349 11.8951 4.48017 11.707 4.29256L7.707 0.292556C7.31598 -0.097909 6.68247 -0.0974613 6.292 0.293556C5.90153 0.684574 5.90198 1.31809 6.293 1.70856L8.586 4.00156H1C0.447715 4.00156 0 4.44927 0 5.00156C0 5.55384 0.447715 6.00156 1 6.00156Z"
                                         fill="white" />
                                 </svg>
-                            </button></a>
+                            </button>
+                        </a>
                     </div>
-
                 </div>
             </div>
 
-
-
-            <div class="flex gap-4 pb-8">
-                <div class="flex flex-col items-center justify-center gap-4 bg-white rounded-lg pl-4 pr-8">
+            <div class="flex flex-col gap-4 lg:flex-row pb-8">
+                <div
+                    class="flex flex-col items-center justify-center gap-4 bg-white rounded-lg p-4 w-full lg:w-auto lg:pr-8 max-md:w-[95%]">
                     <p class="font-assistant text-[18px] font-semibold leading-5 text-midnightblue pt-4">Grafik Pasien
                         berdasarkan Golongan Darah</p>
-
                     <Pie v-if="loaded" :data="BloodData" :options="BloodOptions"
-                        class="border border-lightsilver rounded-lg max-w-[350px] h-full max-h-[350px] text-white ml-8" />
+                        class="border border-lightsilver rounded-lg w-full max-w-[350px] h-full max-h-[350px] text-white" />
                     <div class="pb-4">
-                        <p class="font-opensans font-bold text-black text-[20px]">Golongan Darah Terbanyak: {{ golonganDarahTerbanyak }}</p>
-                        <p class="font-opensans font-bold text-black text-[20px]">Golongan Darah Terlangka: {{ golonganDarahTerlangka }}</p>
+                        <p class="font-opensans font-bold text-black text-[20px]">Golongan Darah Terbanyak: {{
+                            golonganDarahTerbanyak }}</p>
+                        <p class="font-opensans font-bold text-black text-[20px]">Golongan Darah Terlangka: {{
+                            golonganDarahTerlangka }}</p>
                         <p class="font-opensans font-bold text-black text-[20px]">Total : {{ TotalGeneralPatient }}</p>
                     </div>
                 </div>
 
-                <div class="flex flex-col items-center justify-center gap-4 rounded-lg pl-4 pr-8">
-                    <div class="bg-white py-4">
+                <div class="flex flex-col items-center justify-center gap-4 w-full lg:w-auto lg:pr-8 max-md:w-[95%]">
+                    <div class="bg-white py-4 w-full">
                         <p class="font-assistant text-[18px] font-bold leading-5 text-midnightblue w-full pb-2 pl-8">Grafik
-                            Pasien
-                            berdasarkan Umur</p>
+                            Pasien berdasarkan Umur</p>
                         <Bar v-if="loaded" :data="ageData" :options="ageOptions"
-                            class="w-full border border-lightsilver rounded-lg p-4 max-w-[700px] min-h-[340px] max-h-[350px] text-white mx-4" />
+                            class="w-full border border-lightsilver rounded-lg p-4 lg:max-w-[700px] min-h-[340px] max-h-[350px] text-white mx-4" />
                     </div>
-                    <div>
-                        <div class="flex flex-col py-4">
-                            <div class="bg-white rounded-lg shadow-[0_0_12px_0_rgba(0,0,0,0.04)] pl-4 pr-20 flex flex-col">
-                                <p class="font-opensans text-black font-semibold text-[16px] leading-[52px]">
-                                    Total Pasien: {{
-                                        TotalGeneralPatient }}</p>
-                            </div>
-                        </div>
+                    <div class="flex flex-col py-4 w-full">
+                        <div class="bg-white rounded-lg shadow-[0_0_12px_0_rgba(0,0,0,0.04)] p-4 flex flex-col">
+                            <p class="font-opensans text-black font-semibold text-[16px] leading-[52px]">Total Pasien: {{
+                            TotalGeneralPatient }}</p>
                     </div>
-
                 </div>
-
             </div>
-
-
-
         </div>
     </div>
-</template>
+</div></template>
+
