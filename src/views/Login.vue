921886dbe26148cb343c05bf8a544e0c20e69a98
Muhammad Abieb Basnuril <banurilabib@gmail.com>
2024-07-16 17:41:36 +0700

    ✨ feat: Show/hide
    Adding show/hide password into login page

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -11,7 +11,8 @@ export default {
             error: '',
             rememberMe: false,
             rememberedUsername: '',
-            rememberedPassword: ''
+            rememberedPassword: '',
+            showPassword: false
         };
     },
     methods: {
@@ -52,6 +53,9 @@ export default {
             } catch (error) {
                 this.error = 'Terjadi kesalahan, silakan coba lagi.';
             }
+        },
+        togglePasswordVisibility() {
+            this.showPassword = !this.showPassword;
         }
     },
     mounted() {
@@ -73,7 +77,8 @@ export default {
                     class="object-cover w-full h-full"></router-link>
         </div>
         <div class="flex flex-col lg:p-36 md:p-52 sm:20 p-8 w-full lg:w-1/2">
-            <img class="w-[120px] bg-teal rounded-md shadow-[0_15px_28px_6px_rgba(0,0,0,0.25)] mb-20" src="../assets/logoElgekaPutih1.png" alt="Logo">
+            <img class="w-[120px] bg-teal rounded-md shadow-[0_15px_28px_6px_rgba(0,0,0,0.25)] mb-20"
+                src="../assets/logoElgekaPutih1.png" alt="Logo">
             <h1 class="text-2xl font-bold font-verdana text-[32px] mb-4">Dashboard Pengurus</h1>
             <form @submit.prevent="login">
                 <div class="mb-4">
@@ -85,9 +90,35 @@ export default {
                 </div>
                 <div class="mb-4">
                     <label for="password" class="block text-[#344054] mb-2">Password</label>
-                    <input type="password" id="password" name="password" v-model="password"
-                        class="w-full border border-lightgrayish rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 placeholder:text-bluegray placeholder:text-verdana placeholder:leading-5 placeholder:text-[14px]"
-                        autocomplete="off" placeholder="Enter Password">
+                    <div class="relative">
+                        <input :type="showPassword ? 'text' : 'password'" id="password" name="password" v-model="password"
+                            class="w-full border border-lightgrayish rounded-md py-2 px-3 focus:outline-none focus:border-blue-500 placeholder:text-bluegray placeholder:text-verdana placeholder:leading-5 placeholder:text-[14px]"
+                            autocomplete="off" placeholder="Enter Password">
+                        <button type="button" @click="togglePasswordVisibility"
+                            class="absolute inset-y-0 right-0 px-3 flex items-center text-gray-500 focus:outline-none">
+                            <span v-if="showPassword"><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M2 2L22 22" stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                    <path
+                                        d="M6.71277 6.7226C3.66479 8.79527 2 12 2 12C2 12 5.63636 19 12 19C14.0503 19 15.8174 18.2734 17.2711 17.2884M11 5.05822C11.3254 5.02013 11.6588 5 12 5C18.3636 5 22 12 22 12C22 12 21.3082 13.3317 20 14.8335"
+                                        stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                    <path
+                                        d="M14 14.2362C13.4692 14.7112 12.7684 15.0001 12 15.0001C10.3431 15.0001 9 13.657 9 12.0001C9 11.1764 9.33193 10.4303 9.86932 9.88818"
+                                        stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                                </svg></span>
+                            <span v-else><svg width="25px" height="25px" viewBox="0 0 24 24" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path d="M1 12C1 12 5 4 12 4C19 4 23 12 23 12" stroke="#000000" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                    <path d="M1 12C1 12 5 20 12 20C19 20 23 12 23 12" stroke="#000000" stroke-width="2"
+                                        stroke-linecap="round" stroke-linejoin="round" />
+                                    <circle cx="12" cy="12" r="3" stroke="#000000" stroke-width="2" stroke-linecap="round"
+                                        stroke-linejoin="round" />
+                                </svg></span>
+                        </button>
+                    </div>
+
                 </div>
                 <div class="mb-4 flex flex-col">
                     <div>
